1f859905011825d2b89acb9ef772c761dcd26b13
Be-HinD <kr100475@gmail.com>
2025-02-12 03:25:32 +0900

    주석 추가

--- cicd/src/main/java/org/example/cicd/controller/controller.java
+++ cicd/src/main/java/org/example/cicd/controller/controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
 
+    // Test API
     @GetMapping("/test")
     public ResponseEntity<String> testMethod() {
 
